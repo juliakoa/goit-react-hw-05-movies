b5fc9ae8e711a33f07bc26ff19df9345648dd8e6
juliakoa <julisia199706@gmail.com>
2023-08-21 14:59:12 +0200

    br dodany

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Outlet,
-  Link,
-} from 'react-router-dom';
+import { Route, Routes, Outlet, Link } from 'react-router-dom';
 import Home from './Home/Home';
 import Movies from './Movies/Movies';
 import MovieDetails from './MovieDetails/MovieDetails';
